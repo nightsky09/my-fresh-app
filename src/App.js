b14c90cbd9c3f83c0f439d6820402cb11f6e3ebb
Sujal Patel <thesujalpatel09@gmail.com>
2024-06-29 15:30:37 +0530

    Update logo and link

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  className="App-link"
+  href="https://www.propelleraero.com/dirtmate/"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+
       </header>
     </div>
   );
